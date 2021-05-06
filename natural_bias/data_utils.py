6cace7e8aaf51069b68b0506e21948724f6efbe7
yanadranker <drunkeryana@gmail.com>
2021-05-06 11:05:19 +0300

    Added generate_heuristics_file to data_utils - generates a file with all samples from given dataset that obey some heuristic. Currently only MNLI_Binary with HANS heuristic is supported

--- natural_bias/data_utils.py
+++ natural_bias/data_utils.py
@@ -3,6 +3,7 @@ from pathlib import Path
 import requests
 import pandas as pd
 import zipfile
+from dataset_utils import lexical_overlap, is_constituent, is_subsequence
 
 datasets_config = {'SNLI': {'urlpath': 'https://nlp.stanford.edu/projects/snli/snli_1.0.zip',
                             'files': ['snli_1.0_train.txt', 'snli_1.0_dev.txt', 'snli_1.0_test.txt'],
@@ -70,7 +71,7 @@ datafiles_config = {
                              'gold_label'],
                   'filters': {'sentence1': [''], 'sentence2': [''], 'gold_label': ['', '-']}
                   }
-    }
+}
 
 
 def prepare_dataset(dataset='SNLI', force=False):
@@ -179,6 +180,62 @@ def process_tsv(filepath, fields, filters, mappings=None):
         f.truncate()
 
 
+def generate_heuristics_file(dataset='MNLI_Binary', force=False):
+    """
+    For each of the train/val/test of a dataset generate a file that holds all the samples containing
+    a specified heuristic. Currently only implemented for MNLI_Binary with HANS heuristics.
+    The fields of the generated files are:
+    'sample_idx', 'sentence1', 'sentence2', 'gold_label', 'lexical_overlap', 'subsequence', 'constituent'.
+    :param dataset: Currently only MNLI_Binary, can be extended to SNLI
+    :param force: force regeneration of files even if they exist
+    :return: paths to generated files
+    """
+    file_train, file_val, file_test = prepare_dataset(dataset)
+    fields = datasets_config[dataset]['fields']
+    field_indices = [fields.index(field_name) for field_name in ['sentence1', 'sentence1_binary_parse',
+                                                                 'sentence2', 'sentence2_binary_parse', 'gold_label']]
+    heuristics_filepath_list = []
+    os.makedirs('/'.join(['data', 'datafiles']), exist_ok=True)
+    for file in datasets_config[dataset]['files']:
+        filename, file_extension = os.path.splitext(file)
+        heuristics_filepath = '/'.join(['data', 'datafiles', filename + '_heristics' + file_extension])
+        heuristics_filepath_list.append(heuristics_filepath)
+
+    for filepath, heuristics_filepath in zip([file_train, file_val, file_test], heuristics_filepath_list):
+        if os.path.isfile(heuristics_filepath) and not force:
+            continue
+        else:
+            with open(filepath, 'r') as f:
+                lines = f.readlines()
+
+            # remove line ending and split on tabs. Skip first line (headers)
+            samples = []
+            for line in lines[1:]:
+                samp = line.splitlines()[0].split(sep='\t')
+                candidate = tuple([samp[field_idx] for field_idx in field_indices])
+                if len(candidate[0].split()) < 80:
+                    samples.append(candidate)
+
+            samples_with_heuristic = []
+            for ind, samp in enumerate(samples):
+                prem, prem_parse, hyp, hyp_parse, label = samp
+                all_in, _ = lexical_overlap(prem, hyp, norm_by_hyp=False)
+                subsequence_flag = is_subsequence(prem, hyp)
+                constituent_flag = is_constituent(hyp, prem_parse)
+                if any([all_in, subsequence_flag, constituent_flag]):
+                    flags = (all_in, subsequence_flag, constituent_flag)
+                    samples_with_heuristic.append((ind, prem, hyp, label) + flags)
+
+            df = pd.DataFrame(samples_with_heuristic, columns=['sample_idx', 'sentence1', 'sentence2', 'gold_label',
+                                                               'lexical_overlap', 'subsequence', 'constituent'])
+            with open(heuristics_filepath, 'w') as f:
+                f.seek(0)
+                df.to_csv(f, sep='\t', index=False, line_terminator='\n')
+                f.truncate()
+
+    return heuristics_filepath_list
+
+
 if __name__ == "__main__":
     prepare_dataset(dataset='SNLI')
     prepare_dataset(dataset='SNLI_Binary')
@@ -186,3 +243,4 @@ if __name__ == "__main__":
     prepare_dataset(dataset='MNLI_Binary')
     prepare_data_file('HANS')
     prepare_data_file('SNLI_hard')
+    generate_heuristics_file(dataset='MNLI_Binary')
